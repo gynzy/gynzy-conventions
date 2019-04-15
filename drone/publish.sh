16e2f42ef0d56013d4eefa15e7e724a32b2fd2d0
Justin Mol <justin.mol@hotmail.nl>
2019-04-15 13:39:10 +0200

    [YETI-646] dronev1

--- drone/publish.sh
+++ drone/publish.sh
@@ -4,11 +4,8 @@ set -eo pipefail
 
 VERSION=`yarn version --non-interactive 2>/dev/null | grep 'Current version' | grep -o -E '[0-9]+(\.[0-9]+){1,2}$'`
 if [ "$DRONE_BUILD_EVENT" == "pull_request" ]; then
-	PR=`cut -d: -f1 <<< $DRONE_COMMIT_REFSPEC`
-	HASH=${DRONE_BUILD_NUMBER}
-
-	PUBLISHVERSION="$VERSION-$PR.$HASH"
-	TAG=$PR
+	PUBLISHVERSION="$VERSION-$DRONE_SOURCE_BRANCH.$DRONE_BUILD_NUMBER"
+	TAG=$DRONE_SOURCE_BRANCH
 elif [ "$DRONE_BUILD_EVENT" == "tag" ]; then
 	if [ "$DRONE_TAG" != "$VERSION" ]; then
 		exit 1
@@ -18,5 +15,7 @@ elif [ "$DRONE_BUILD_EVENT" == "tag" ]; then
 else
 	exit 1
 fi
+
+echo "Publishing package with tag: $TAG and version: $PUBLISHVERSION";
 
 yarn publish --non-interactive --no-git-tag-version --tag "$TAG" --new-version "$PUBLISHVERSION"
